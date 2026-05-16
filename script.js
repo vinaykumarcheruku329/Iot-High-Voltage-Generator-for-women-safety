6f2af13c5958a364175cb75aadd9bbca21cee9d8
vinaykumarcheruku329 <vinaykumarcheruku329@gmail.com>
2026-05-16 20:55:55 +0530

    Add files via upload
    Iot-High-Voltage-Generator-for-women-safety

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function login() {
-    window.location.href = "dashboard.html";
+
+    let username = document.querySelector('input[type="text"]').value;
+
+    let password = document.querySelector('input[type="password"]').value;
+
+    let correctUsername = "vinaykumar";
+
+    let correctPassword = "9848";
+
+    if(username === correctUsername && password === correctPassword) {
+
+        alert("Login Successful");
+
+        window.location.href = "dashboard.html";
+
+    }
+    else {
+
+        alert("Invalid Username or Password");
+    }
 }
